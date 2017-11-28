104066a3009bac8492880a11ae5c7b36506d3b46
Amanda Ackerman <ackerm44@gmail.com>
2017-11-28 00:33:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,7 +37,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.id = characters.series_id
   GROUP BY series.title
-  ORDER BY characters.species
+  ORDER BY characters.species DESC LIMIT 1
   "
 end
 
